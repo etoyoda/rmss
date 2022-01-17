d175982b42bac5100f4a4bcefb7eebfbdc1bd6c8
TOYODA Eizi <toyoda.eizi@gmail.com>
2022-01-17 00:09:21 +0000

    recvmsgをリピートするようにすれば長大メッセージ送受ができている。1MBまで実証。

--- study/unix2size.rb
+++ study/unix2size.rb
@@ -1,6 +1,8 @@
 # UNIX ドメインソケットの読み書きをする練習
 # 一度の sendmsg/recvmsg で送受信できる限界を越えた大きさのメッセージを送受
 # する試験
+# 1 MB までのメッセージについて試験した。210 kB を越えると recvmsg は
+# 複数回に分けて受信する必要がある。
 
 require 'socket'
 
@@ -17,17 +19,25 @@ end
 def recvbull(sock)
   shdr = sock.recvmsg(10)
   raise Errno::EMSGSIZE unless shdr[0].size == 10
-  bullsize = shdr[0][0,10].to_i
-  bull = sock.recvmsg(bullsize)
-  raise Errno::EMSGSIZE unless bull[0].size == bullsize
-  bull[0]
+  bullsize = shdr.first[0,10].to_i
+  recvsize = bullsize
+  buf = []
+  loop {
+    resp = sock.recvmsg(recvsize)
+    msg = resp.first
+    buf.push msg
+    recvsize -= msg.size
+    break if recvsize.zero?
+    STDERR.puts "retrying remaining #{recvsize}"
+  }
+  buf.join
 end
 
 def parent(sockname, cpid)
   UNIXServer.open(sockname) {|server|
     sock = server.accept
     msg = "x" * 1024
-    10.times {
+    11.times {
       sendbull(sock, msg)
       msg = msg * 2
     }
@@ -38,7 +48,7 @@ end
 
 def child sockname
   UNIXSocket.open(sockname) {|sock|
-    10.times {
+    11.times {
       bull = recvbull(sock)
       STDERR.printf("%u bytes received\n", bull.size)
     }
