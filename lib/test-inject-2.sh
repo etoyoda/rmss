40be087b49f9ccf7fbcc9f5298560bdc94081add
TOYODA Eizi <toyoda.eizi@gmail.com>
2022-01-29 23:29:05 +0900

    サーバ起動が遅い場合に落ちる対策（#1 と同件）

--- lib/test-inject-2.sh
+++ lib/test-inject-2.sh
@@ -3,5 +3,6 @@
 sock=/tmp/rmss.test1.sock
 
 ruby -I. rmss-print.rb -once jmasock://localhost:3776 &
+sleep 1
 echo foo | ruby -I. rmss-inject.rb jmasock://localhost:3776
 wait
--- lib/test-inject-3.sh
+++ lib/test-inject-3.sh
@@ -5,5 +5,6 @@ sock=/tmp/rmss.test1.sock
 host='[::]'
 
 ruby -I. rmss-print.rb -once jmasock://${host}:3776/ &
+sleep 1
 echo foo | ruby -I. rmss-inject.rb jmasock://${host}:3776/
 wait
